79af0e25c8a5dde6ba993e75ab637bd772a7de7d
kamankam <kamanovae@gmail.com>
2022-06-02 13:56:58 +0700

    add svg component

--- src/components/task/index.ts
+++ src/components/task/index.ts
@@ -1,3 +1,6 @@
 import Tasks from './Tasks'
+import DoneIcon from './svg/DoneIcon'
+import TrashIcon from './svg/TrashIcon'
+import CalendarIcon from './svg/CalendarIcon'
 
-export { Tasks }
+export { Tasks, DoneIcon, TrashIcon }
